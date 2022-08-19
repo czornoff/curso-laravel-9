b81039cf63af0279b33f8fc15a8146a82c5beec2
Christian Zornoff <czornoff@gmail.com>
2022-08-19 18:26:46 -0300

    teste

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Editar Usuário')
+
+@section('content')
+    <h1>Editar Usuário {{$user->name}}</h1>
+    
+    @include('includes.validations-form.blade')
+    
+    <form action="{{ route('users.update', $user->id) }}" method="post">
+        @method('PUT')
+        @include('users._partials.form')
+    </form>
+@endsection
